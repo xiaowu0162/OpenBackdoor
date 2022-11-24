b6d69f9e65661b580403c753bd516bd868908670
Di Wu <xiaowu200031@gmail.com>
2022-11-23 19:14:39 -0800

    update

--- openbackdoor/data/nli_dataset.py
+++ openbackdoor/data/nli_dataset.py
@@ -32,6 +32,56 @@ class MnliProcessor(DataProcessor):
                 
         return examples
 
+class QnliProcessor(DataProcessor):
+    def __init__(self):
+        super().__init__()
+        self.path = "./datasets/NLI/qnli"
+        self.text2lab = {'entailment': '0', 'not_entailment': '1'}
+        
+    def get_examples(self, data_dir, split):
+        if split == 'test':
+            split = 'dev'
+        if data_dir is None:
+            data_dir = self.path
+        path = os.path.join(data_dir, "{}.tsv".format(split))
+        examples = []
+        with open(path, encoding='utf8') as f:
+            _ = f.readline()
+            for line in f.readlines():
+                idx, context, hypothesis, label = line.strip().split('\t')
+                text_a = context
+                text_b = hypothesis
+                example = (text_a+" "+text_b, int(self.text2lab[label]), 0)
+                examples.append(example)
+
+        return examples
+
+class AnliProcessor(DataProcessor):
+    def __init__(self):
+        super().__init__()
+        self.path = "./datasets/NLI/qnli"
+        self.text2lab = {'c': '0', 'e': '1', 'n': '2'}
+        
+    def get_examples(self, data_dir, split):
+        if data_dir is None:
+            data_dir = self.path
+        path = os.path.join(data_dir, "{}.jsonl".format(split))
+        examples = []
+        with open(path, encoding='utf8') as f:
+            for line in f.readlines():
+                entry = json.loads(line.strip())
+                label, context, hypothesis = self.text2lab[entry['label']], entry['context'], entry['hypothesis']
+                text_a = context
+                text_b = hypothesis
+                example = (text_a+" "+text_b, int(label), 0)
+                # example = (text_a+" "+text_b, int(label), 0)
+                examples.append(example)
+                
+        return examples
+
+     
 PROCESSORS = {
     "mnli" : MnliProcessor,
+    "anli" : AnliProcessor,
+    "qnli" : QnliProcessor,
 }
