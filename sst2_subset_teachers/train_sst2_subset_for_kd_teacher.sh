2d0d02c2a92727a668240dec549d4deaaa9878de
Di Wu <xiaowu200031@gmail.com>
2022-11-25 19:15:23 -0800

    update

--- sst2_subset_teachers/train_sst2_subset_for_kd_teacher.sh
+++ sst2_subset_teachers/train_sst2_subset_for_kd_teacher.sh
@@ -5,11 +5,13 @@ export CUDA_VISIBLE_DEVICES=1
 
 
 TASK_NAME=sst2
-MAX_TRAIN_DATA=100
+MAX_TRAIN_DATA=1000
 
+# --model_name_or_path "textattack/bert-base-uncased-imdb"
+# use LR ~1e-5 if start from imdb; use LR ~5e-5 if from pre-trained BERT
 
 python train_sst2_subset_for_kd_teacher.py \
-  --model_name_or_path "textattack/bert-base-uncased-imdb" \
+  --model_name_or_path "bert-base-uncased" \
   --task_name $TASK_NAME \
   --do_train \
   --max_train_samples ${MAX_TRAIN_DATA} \
@@ -19,8 +21,8 @@ python train_sst2_subset_for_kd_teacher.py \
   --save_total_limit 0 \
   --max_seq_length 128 \
   --per_device_train_batch_size 32 \
-  --learning_rate 1e-5 \
-  --num_train_epochs 10 \
+  --learning_rate 5e-5 \
+  --num_train_epochs 20 \
   --load_best_model_at_end \
   --metric_for_best_model "accuracy" \
   --output_dir "${TASK_NAME}_${MAX_TRAIN_DATA}data_checkpoints"
